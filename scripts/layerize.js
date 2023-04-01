79000653f381dd5a8f3f38fbd6e409ff07e3fdf5
12Me21 <12Me21.mc@gmail.com>
2023-04-01 16:44:02 -0400

    bar

--- scripts/layerize.js
+++ scripts/layerize.js
@@ -12,38 +12,12 @@ let layers = new Map()
 let glyphs = []
 let x = performance.now()
 let w1 = 0, w2 = edata.length, ws = -1
-process.stderr.write("["+".".repeat(100)+"]\x1B[2G")
-let tip, len = 0
+process.stderr.write("\n\x1B[A💣\\"+"_".repeat(97)+"\x1B7")
 
 for (let em of edata) {
-	let ws2 = Math.round(w1/w2*800)
+	let ws2 = Math.round(w1/w2*100)
 	if (ws2!=ws) {
-		let tip2 = ws2 % 8
-		let len2 = ws2 / 8 | 0
-		if (len2 > len) {
-			if (tip) {
-				process.stderr.write("\b")
-				tip = null
-			}
-			process.stderr.write("█".repeat(len2-len))
-			len = len2
-			if (tip2) {
-				process.stderr.write(" ▏▎▍▌▋▊▉"[tip2])
-				tip = tip2
-			}
-		} else {
-			if (tip != tip2) {
-				if (tip) {
-					process.stderr.write("\b")
-					tip = null
-				}
-				if (tip2) {
-					process.stderr.write(" ▏▎▍▌▋▊▉"[tip2])
-					tip = tip2
-				}
-			}
-		}
-		//process.stderr.write("|")
+		process.stderr.write("\x1B8\b\x1B7🔥\n")
 		ws = ws2
 	}
 	w1++ // is there a builtin for progress bars
@@ -51,6 +25,9 @@ for (let em of edata) {
 	glyphs.push({glyphName:em.glyphName, codes:em.codes, vs16:em.vs16})
 	if (!em.file)
 		continue
+	
+	//if (w1/w2>0.5)
+	//	throw new Error('x')
 	
 	// todo: would be nice if we could like, tell it to start loading the next file right away so it will be ready by the time the current one processes.
 	// also; would be nice if we could sync this task with fontforge so it imports the layers as we produce them
@@ -78,9 +55,10 @@ for (let em of edata) {
 	em.layers = ls
 	//process.stderr.write("shapes:"+sc+"layers:"+paths.length+"new:"+nl+"\n")
 }
+process.stderr.write('\x1B8💥\n')
 //console.log(layers)
 let x2 = performance.now()
-console.warn('\n',x2-x)
+console.warn(x2-x)
 process.exit(0)
 
 let meta = edata.map(em=>{
