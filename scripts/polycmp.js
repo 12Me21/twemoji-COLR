9f9e8d35ef3793066520c8e369dfc82bc270436d
12Me21 <12Me21.mc@gmail.com>
2023-04-06 15:28:34 -0400

    idk

--- scripts/polycmp.js
+++ scripts/polycmp.js
@@ -4,8 +4,12 @@ class One {
 	constructor(name) {
 		this.name = name
 		this.text = Fs.readFileSync(dir+"/"+name, 'utf-8')
-		this.text = this.text.replace(/(<path) ([^>]*") (fill="[^">]+")/g, (m,a,b,c)=>{
-			return a + " " + c + " " + b
+		this.text = this.text.replace(/(<[a-zA-Z]+ )[^>]*?([/]?>)/g, (m,b,a)=>{
+			let attrs = m.match(/[-a-zA-Z]+="[^">]*"/g)
+			if (!attrs.some(x=>x.startsWith('fill')))
+				attrs.push('fill="inherit"')
+			attrs.sort()
+			return b+attrs.join(" ")+a
 		})
 		this.i = 0
 	}
@@ -33,13 +37,18 @@ let dir = process.argv[2]
 let files = Fs.readdirSync(dir)
 files = files.map(f=>new One(f))
 
+let out = ""
+
 for (let i=0; ; i++) {
 	let cs = files.map(f=>f.scan())
 	let c = cs[0]
 	for (let j=1; j<files.length; j++) {
 		if (cs[j] != c) {
 			console.warn(cs)
+			console.log(out)
 			throw new Error('file contents mismatch: '+files[0].name+' and '+files[j].name)
+			//out += files[0].text.slice(files[0].i)
+			//c = undefined
 		}
 	}
 	if (!c)
@@ -48,11 +57,15 @@ for (let i=0; ; i++) {
 	if (c=='"') {
 		let ss = files.map(f=>f.readString())
 		if (same(ss)) {
-			process.stdout.write('"'+ss[0]+'"')
+			out += '"'+ss[0]+'"'
 		} else {
-			process.stdout.write('"{'+ss.join("|")+'}"')
+			if (ss[0].length > 10)
+				ss = ss.map((s,i)=>i!=3 && s==ss[3] ? "@3" : s)
+			out += '"{'+ss.join("|")+'}"'
 		}
 	} else
-		process.stdout.write(c)
+		out += c
 }
 
+//console.log(out)
+Fs.writeFileSync(dir.replace(/[/]?$/,".svg"), out)
