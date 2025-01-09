0cbb026e8299596411c8a5328fa604cea9c199fa
12Me21 <12Me21.mc@gmail.com>
2025-01-08 16:49:29 -0800

    fix metrics

--- Makefile
+++ Makefile
@@ -11,11 +11,15 @@ build/Twemoji.otf: build/glyphs.otf build/cpal.ttx build/colr.ttx data/import.tt
 data/edata.mjs: data/emoji-test.txt scripts/parse-emoji-test.js
 	node scripts/parse-emoji-test.js >data/edata.mjs
 
-build/colr.ttx build/cpal.ttx: data/layers.mjs scripts/make-colr.js scripts/xml.js
+build/colr.ttx build/cpal.ttx: build/layers.mjs scripts/make-colr.js scripts/xml.js
 	node scripts/make-colr.js
 
-data/layers.mjs build/glyphs.json: data/edata.mjs scripts/layerize.js scripts/xml.js
+build/layers.mjs build/glyphs.json: data/edata.mjs scripts/layerize.js scripts/xml.js
 	node scripts/layerize.js
 
+# todo: i would like to split this one into 2 steps
+#  so we can adjust metrics without having to re-import all the layers.
+# 1: create glyphs, import the layers, simplify outlines, etc.
+# 2: apply metrics to that font file (which does involve shifting the outlines around, to set the descent/bearings)
 build/glyphs.otf: build/glyphs.json scripts/fontforge.py
 	fontforge -script scripts/fontforge.py <build/glyphs.json
--- scripts/fontforge.py
+++ scripts/fontforge.py
@@ -6,18 +6,16 @@ import json
 
 glyphList = json.load(sys.stdin)
 
-SCALE = 24 # upscale factor for svg units
 VIEWBOX = 36 # viewbox of twemoji svgs
 WATERLINE = 8 # how far up the baseline should be (in svg units)
 MARGIN = 1 # left/right bearing, in svg units
-# basic font metrics
-EM = 32 * SCALE
-ASCENT = round(0.8 * SCALE)
-DESCENT = EM - ASCENT
-WIDTH = (MARGIN+VIEWBOX+MARGIN) * SCALE
+EMOJI_SCALE = 1.125 # in `em` units. i.e. at a font size of 16px, emojis will be 18px
 
-# transform to apply to imported svg files
-IMPORT_TRANSFORM = [SCALE,0,0,SCALE,MARGIN*SCALE,-(ASCENT-VIEWBOX)*SCALE - WATERLINE*SCALE]
+SCALE = 24 # upscale factor for svg units
+
+EM = round((VIEWBOX / EMOJI_SCALE) * SCALE)
+DESCENT = round(0.2 * EM) # doesnt really matter for display itself, but we do this to match other fonts
+WIDTH = round((MARGIN+VIEWBOX+MARGIN) * SCALE)
 
 f = fontforge.font()
 f.encoding = 'UnicodeFull'
@@ -28,9 +26,11 @@ f.familyname = "Twitter Color Emoji"
 f.fullname = "Twitter Color Emoji"
 f.os2_vendor = "12;;"
 
-f.em = EM
-f.descent = DESCENT
-f.ascent = ASCENT
+# temporary metrics, to facilitate importing the svgs
+f.em = VIEWBOX * SCALE
+f.ascent = 0
+f.descent = VIEWBOX * SCALE
+
 f.upos = -108 # idk ?? nothing uses this anyway
 f.uwidth = 2 * SCALE
 
@@ -43,8 +43,8 @@ f.hhea_descent_add = False
 
 f.os2_winascent = (VIEWBOX-WATERLINE)*SCALE + SCALE
 f.os2_windescent = (WATERLINE)*SCALE + SCALE
-f.os2_typoascent = round(712/768*EM) # idr how i calculated these, but i did
-f.os2_typodescent = round(-188/768*EM)
+f.os2_typoascent = f.os2_winascent #round(712/768*EM) # idr how i calculated these, but i did. UPDATE: i got them from the hhea ascent/descent values in my copy of Roboto.
+f.os2_typodescent = -f.os2_windescent # round(-188/768*EM)
 f.os2_typolinegap = 0
 f.hhea_ascent = f.os2_typoascent
 f.hhea_descent = f.os2_typodescent
@@ -82,11 +82,14 @@ def create_ligature(codes):
 
 def create_layer(name, shapecount):
 	glyph = f.createChar(-1, name)
+	# load each shape in the layer, being sure to *individually* correct their directions, otherwise removeOverlap() will break
 	for i in range(0, shapecount):
-		glyph.importOutlines('build/layers/'+name+"_"+str(i)+".svg", simplify=False, correctdir=True, scale=False)
-	glyph.transform(IMPORT_TRANSFORM)
+		glyph.importOutlines('build/layers/'+name+"_"+str(i)+".svg", simplify=False, correctdir=True, scale=True)
+	glyph.transform([1,0,0,1,MARGIN*SCALE,(VIEWBOX-WATERLINE)*SCALE])
+	# merge all the shapes together
 	glyph.removeOverlap()
 	glyph.correctDirection()
+	# apply various simplifications
 	glyph.simplify(0.5, ('removesingletonpoints', 'choosehv', 'smoothcurves', 'ignoreextrema'), 0.2, 7.2, 7.2)
 	glyph.round()
 	glyph.simplify(0.25, ('removesingletonpoints', 'choosehv', 'smoothcurves', 'ignoreextrema'), 0.2, 7.2, 7.2)
@@ -107,6 +110,11 @@ for g in glyphList:
 	else:
 		create_layer(name, g['shapeCount'])
 
+# now set the real metrics. (be careful so fontforge doesn't re-scale the entire font)
+f.ascent = EM - DESCENT
+f.descent = DESCENT
+assert f.em == EM
+		
 f.generate("build/glyphs.otf", flags=('opentype', 'round', 'no-hints', 'no-flex', 'short-post'))
 
 # todo: use setTableData to create cpal/colr
