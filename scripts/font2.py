8e705f864527655de8544c040aafaac4c064972f
12Me21 <12Me21.mc@gmail.com>
2025-02-03 14:22:44 -0800

    back.

--- scripts/font2.py
+++ scripts/font2.py
@@ -72,7 +72,7 @@ couples = {
 	"wh": (person_list, gnames("‍❤"), gnames("‍"), person_list),
 }
 
-# handle hardcoded couple emojis
+# destroy couple emojis !!
 f.addLookup('decouple', 'gsub_multiple', None, [("ccmp",[("DFLT",["dflt"])])], 'any')
 f.addLookupSubtable('decouple', 'decouple-1')
 
@@ -111,37 +111,13 @@ def create_couple(glyph, cdata):
 glyphList = json.load(open('build/glyphs.json'))
 for g in glyphList:
 	name = str(g['glyphName'])
-	#if 'decouple' in g:
+	if 'decouple' in g:
 		# explode and kill them !!!
-		#glyph = f[name]
-		#glyph.addPosSub('decouple-1', g['decouple'])
+		glyph = f[name]
+		glyph.addPosSub('decouple-1', g['decouple'])
 	if 'couple' in g:
 		glyph = f.createChar(-1, name)
 		create_couple(glyph, g['couple'])
-
-f.addLookup("hc_1", 'gsub_ligature', None, [("ccmp",[("DFLT",["dflt"])])])
-f.addLookupSubtable("hc_1", "hc_1b")
-f.addLookup("hc_2", 'gsub_multiple', None, ())
-f.addLookupSubtable("hc_2", "hc_2b")
-
-decouple = json.load(open('data/decouple.json'))
-
-for source in decouple:
-	# we want to create:
-	# in hc_1, map source -> left half
-	# in hc_2, map left half -> left half + right half
-	couple = decouple[source]
-	parts = [f"couple_{couple['type']}_{lname([ord(c) for c in person])}_left" for person in couple['people']]
-	left = f[parts[0]]
-	left.addPosSub("hc_1b", gnames(source))
-	right = f[parts[0]]
-	right.addPosSub("hc_1b", gnames(source))
-	
-# ok so what if, e.g.
-# match "🤝🏽" -> activate 2 substitutions
-# 1: 🤝🏽 -> (the left half)
-# 2: (the left half) -> (the left half) + (the right half)
-	# WAIT no this all can't work. because we have the holding hands emojis where the left and right people might be different gender.. ugh
 
 left_all = []
 right_all = []
--- scripts/parse-emoji-test.js
+++ scripts/parse-emoji-test.js
@@ -63,21 +63,91 @@ for (let i=0;i<26;i++) {
 // e.g. 6 means that the character always appears with either a skin tone modifier or a varation selector 16
 let varsel = {__proto__:null}
 
-import hardcoded_couples from '../data/decouple.json' with {type:'json'}
+let decouples = {
+	"👭":["👩","‍","🤝","‍","👩"],
+	"👭🏻":["👩🏻","‍","🤝","‍","👩🏻"],
+	"👭🏼":["👩🏼","‍","🤝","‍","👩🏼"],
+	"👭🏽":["👩🏽","‍","🤝","‍","👩🏽"],
+	"👭🏾":["👩🏾","‍","🤝","‍","👩🏾"],
+	"👭🏿":["👩🏿","‍","🤝","‍","👩🏿"],
+	"👫":["👩","‍","🤝","‍","👨"],
+	"👫🏻":["👩🏻","‍","🤝","‍","👨🏻"],
+	"👫🏼":["👩🏼","‍","🤝","‍","👨🏼"],
+	"👫🏽":["👩🏽","‍","🤝","‍","👨🏽"],
+	"👫🏾":["👩🏾","‍","🤝","‍","👨🏾"],
+	"👫🏿":["👩🏿","‍","🤝","‍","👨🏿"],
+	"👬":["👨","‍","🤝","‍","👨"],
+	"👬🏻":["👨🏻","‍","🤝","‍","👨🏻"],
+	"👬🏼":["👨🏼","‍","🤝","‍","👨🏼"],
+	"👬🏽":["👨🏽","‍","🤝","‍","👨🏽"],
+	"👬🏾":["👨🏾","‍","🤝","‍","👨🏾"],
+	"👬🏿":["👨🏿","‍","🤝","‍","👨🏿"],
+	"💏":["🧑","‍","❤","‍","💋","‍","🧑"],
+	"💏🏻":["🧑🏻","‍","❤","‍","💋","‍","🧑🏻"],
+	"💏🏼":["🧑🏼","‍","❤","‍","💋","‍","🧑🏼"],
+	"💏🏽":["🧑🏽","‍","❤","‍","💋","‍","🧑🏽"],
+	"💏🏾":["🧑🏾","‍","❤","‍","💋","‍","🧑🏾"],
+	"💏🏿":["🧑🏿","‍","❤","‍","💋","‍","🧑🏿"],
+	"💑":["🧑","‍","❤","‍","🧑"],
+	"💑🏻":["🧑🏻","‍","❤","‍","🧑🏻"],
+	"💑🏼":["🧑🏼","‍","❤","‍","🧑🏼"],
+	"💑🏽":["🧑🏽","‍","❤","‍","🧑🏽"],
+	"💑🏾":["🧑🏾","‍","❤","‍","🧑🏾"],
+	"💑🏿":["🧑🏿","‍","❤","‍","🧑🏿"],
+	/*"🤝":["🫱","‍","🫲"],
+	"🤝🏻":["🫱🏻","‍","🫲🏻"],
+	"🤝🏼":["🫱🏼","‍","🫲🏼"],
+	"🤝🏽":["🫱🏽","‍","🫲🏽"],
+	"🤝🏾":["🫱🏾","‍","🫲🏾"], don't decouple these i guess, because the matching skintone handshakes are a special case where they shift the color of one hand to make it stand out more
+	"🤝🏿":["🫱🏿","‍","🫲🏿"],*/
+}
 
-let halfs = {}
-for (let [k,v] of Object.entries(hardcoded_couples)) {
-	halfs[v.people[0]]
+let hardcoded_couples = {
+	"👭":"👩‍🤝‍👩",
+	"👭🏻":"👩🏻‍🤝‍👩🏻",
+	"👭🏼":"👩🏼‍🤝‍👩🏼",
+	"👭🏽":"👩🏽‍🤝‍👩🏽",
+	"👭🏾":"👩🏾‍🤝‍👩🏾",
+	"👭🏿":"👩🏿‍🤝‍👩🏿",
+	"👫":"👩‍🤝‍👨",
+	"👫🏻":"👩🏻‍🤝‍👨🏻",
+	"👫🏼":"👩🏼‍🤝‍👨🏼",
+	"👫🏽":"👩🏽‍🤝‍👨🏽",
+	"👫🏾":"👩🏾‍🤝‍👨🏾",
+	"👫🏿":"👩🏿‍🤝‍👨🏿",
+	"👬":"👨‍🤝‍👨",
+	"👬🏻":"👨🏻‍🤝‍👨🏻",
+	"👬🏼":"👨🏼‍🤝‍👨🏼",
+	"👬🏽":"👨🏽‍🤝‍👨🏽",
+	"👬🏾":"👨🏾‍🤝‍👨🏾",
+	"👬🏿":"👨🏿‍🤝‍👨🏿",
+	"💏":"🧑‍❤️‍💋‍🧑",
+	"💏🏻":"🧑🏻‍❤️‍💋‍🧑🏻",
+	"💏🏼":"🧑🏼‍❤️‍💋‍🧑🏼",
+	"💏🏽":"🧑🏽‍❤️‍💋‍🧑🏽",
+	"💏🏾":"🧑🏾‍❤️‍💋‍🧑🏾",
+	"💏🏿":"🧑🏿‍❤️‍💋‍🧑🏿",
+	"💑":"🧑‍❤️‍🧑",
+	"💑🏻":"🧑🏻‍❤️‍🧑🏻",
+	"💑🏼":"🧑🏼‍❤️‍🧑🏼",
+	"💑🏽":"🧑🏽‍❤️‍🧑🏽",
+	"💑🏾":"🧑🏾‍❤️‍🧑🏾",
+	"💑🏿":"🧑🏿‍❤️‍🧑🏿",
+	"🤝":"🫱‍🫲",
+	"🤝🏻":"🫱🏻‍🫲🏻",
+	"🤝🏼":"🫱🏼‍🫲🏼",
+	"🤝🏽":"🫱🏽‍🫲🏽",
+	"🤝🏾":"🫱🏾‍🫲🏾",
+	"🤝🏿":"🫱🏿‍🫲🏿",
 }
 
 function decode_couple(str) {
-	if (hardcoded_couples[str])
-		return hardcoded_couples[str]
+	str = hardcoded_couples[str] || str
 	let m
 	m = /^(🫱[🏻-🏿]?)‍(🫲[🏻-🏿]?)$/u.exec(str)
 	if (m) {
 		let [_,person1,person2] = m
-		return {type:'hs', people:[person1, person2]}
+		return {type:'hs',people:[person1, person2]}
 	}
 	m = /^([🧑👨👩][🏻-🏿]?)‍(🤝|❤️‍💋|❤️)‍([🧑👨👩][🏻-🏿]?)$/u.exec(str)
 	if (m) {
@@ -134,7 +204,7 @@ for await (let line of lines('data/emoji-test.txt')) {
 			})
 		}
 		// we need to keep the hardcoded versions, because they will appear in the wild and need to be decomposed
-		if (codes.length > 1)
+		if (codes.length > 2)
 			continue
 		// todo: for these, we can reuse the layers from the halfcouple glyphs. this is probably already the case due to regular layer reuse, however if we're tricky we can like, overlap  in the COLR table.
 		// like say,  holding hands (man, left half) followed by holding hands (man, right half). then, the men holding hands can refer to that whole span of the colr table.
@@ -146,12 +216,9 @@ for await (let line of lines('data/emoji-test.txt')) {
 	}
 	data.name = name
 	data.emoji = str
-	/*if (hardcoded_couples[str]) {
-		let couple = hardcoded_couples[str]
-		data.decouple = couple.people.map((p,i)=>{
-			return couple_half_gname([couple.type, gname([...p].map(x=>x.codePointAt())), ['left','right'][i]])
-		})
-	}*/
+	if (decouples[str]) {
+		data.decouple = decouples[str].map(x=>gname([...x].map(x=>x.codePointAt())))
+	}
 	if (codes2.length==1) {
 		data.encoding = [+(codes2[0]), null]
 	} else {
@@ -167,10 +234,6 @@ function gname(codes) {
 			return u
 		return "u"+u
 	}).join("_")
-}
-
-function couple_half_gname([type, pers, half]) {
-	return `couple_${type}_${pers}_${half}`
 }
 
 process.stdout.write("[")
@@ -193,7 +256,8 @@ for (let data of emojis) {
 }
 
 for (let data of couples) {
-	data.glyphName = couple_half_gname(data.couple)
+	let [type, pers, half] = data.couple
+	data.glyphName = `couple_${type}_${pers}_${half}`
 	print_item(data)
 }
 
